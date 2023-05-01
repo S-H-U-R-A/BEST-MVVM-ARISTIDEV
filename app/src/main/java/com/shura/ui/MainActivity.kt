52c847f0e47a76fe2575f1de5ea9c2e6e26b8c65
Sergio <sandres0190@gmail.com>
2023-05-01 11:11:00 -0500

    Primera Versión de uso de DataStore con Hilt

---
+++ app/src/main/java/com/shura/ui/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.shura.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.shura.mvvmaris.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    val binding get() =  _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initListeners()
+
+    }
+
+    private fun initListeners() {
+
+        binding.btnDataStore.setOnClickListener {
+
+            Intent(
+                this,
+                PruebaDataStore::class.java
+            ).also {
+                startActivity(it)
+            }
+
+        }
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
